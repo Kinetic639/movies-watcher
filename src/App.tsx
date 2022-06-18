a88fbcdcbe6da69168ff9778489372595f8408f8
Kinetic639 <michal.stepien36@gmail.com>
2022-06-18 20:33:01 +0200

    added basic material ui navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import {Routes, Route, useLocation, Navigate} from 'react-router-dom'
 import {Box, Typography} from "@mui/material";
+import {NavBar} from "./components/NavBar/NavBar";
 
 export const App = () =>  {
     // const location = useLocation()
   return (
       <Box component='main'>
+          <NavBar/>
           <Typography variant="h6" gutterBottom component="div" > Test</Typography>
       </Box>
 
